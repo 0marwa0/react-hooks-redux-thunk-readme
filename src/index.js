762dc9f7c3638624609724a453cc532c4f66e71a
marwa <marwajawad19@gmail.com>
2021-08-29 08:04:28 -0400

    adding redux thunk

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore,applyMiddleware } from "redux";
 import App from "./App";
 import rootReducer from "./reducer";
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
+import  thunkMiddleware from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension";
+// const store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
+const store = createStore(rootReducer,applyMiddleware(thunkMiddleware))
 ReactDOM.render(
   <Provider store={store}>
     <App />
