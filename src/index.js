dd8ad44dd614989a3d938da2638edad0d4accd2a
marwa <marwajawad19@gmail.com>
2021-08-29 08:39:33 -0400

    adding devtools

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { composeWithDevTools } from "redux-devtools-extension";
 //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
-const store = createStore(rootReducer,applyMiddleware(thunkMiddleware))
+const store = createStore(rootReducer,composeWithDevTools)
 ReactDOM.render(
   <Provider store={store}>
     <App />
